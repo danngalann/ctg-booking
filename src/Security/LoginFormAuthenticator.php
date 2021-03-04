85ce7a5cd4e258f5f9de31e0d30480b1d651de49
danngalann <47415745+danngalann@users.noreply.github.com>
2021-03-04 14:49:30 +0100

    Clean up ignored files

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -96,8 +96,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        return new RedirectResponse($this->urlGenerator->generate(''));
+        return new RedirectResponse($this->urlGenerator->generate('')); // TODO: Redirect somewhere
     }
 
     protected function getLoginUrl()
